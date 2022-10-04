75a7511eb1dccd3a4ce927c88b3c6febf9432c87
Pratyush Motha <67197988+Pratyush1211@users.noreply.github.com>
2022-10-05 04:03:21 +0530

    Initial commit

---
+++ parking_lot/generic_details.py
@@ -0,0 +1,30 @@
+from abc import abstractmethod
+
+
+class VehicleSize:
+    MOTORCYCLE = 0
+    COMPACT = 1
+    LARGE = 2
+
+    
+class Vehicle:
+    def __init__(self, vehicle_size, licence_plate_no, spot_size) -> None:
+        self.vehicle_size = vehicle_size
+        self.licence_plate_no = licence_plate_no
+        self.spot_size = spot_size
+        self.spots_taken = []
+
+    def clear_spots(self):
+        for spots in self.spots_taken:
+            spots.remove_vehicle(self)
+        self.spots_taken = []
+    
+    def take_spot(self, spot):
+        self.spots_taken.append(spot)
+    
+    @abstractmethod
+    def can_fit_in_spot(self,spot):
+        pass
+
+
+        
